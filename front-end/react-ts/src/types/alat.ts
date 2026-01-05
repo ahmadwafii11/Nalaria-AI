40d81c43ee338bac6ab74ac263677889c25ce7a6
ahmadwafii11 <wafiahmad950@gmail.com>
2026-01-06 04:40:09 +0700

    add icon type data with export type IconType

--- front-end/react-ts/src/types/alat.ts
+++ front-end/react-ts/src/types/alat.ts
@@ -1,6 +1,19 @@
+export type IconType=
+ | "wordToPDF"
+ | "mergePDF"
+ | "splitPDF"
+ | "compressPDF"
+ | "jpgToPDF"
+ | "excelToPDF"
+ | "pdfToWord"
+ | "pdfToJPG"
+ | "pdfToExcel"
+ | "removeBackground"
+
 export type AlatItem = {
     id: string;
     name: string;
     desc: string;
-    category?: "PDF" | "Word" | "Excel" | "JPG"
+    category?: "PDF" | "Word" | "Excel" | "JPG";
+    icon: IconType;
 };
