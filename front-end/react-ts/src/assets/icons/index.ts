a608d1de2c5edd50b8e00908697e2ab6ad74d3fa
ahmadwafii11 <wafiahmad950@gmail.com>
2026-01-06 04:36:58 +0700

    add icon tools and index.ts for ICON MAP data

---
+++ front-end/react-ts/src/assets/icons/index.ts
@@ -0,0 +1,23 @@
+import wordToPDF from "./icon word to pdf.svg"
+import mergePDF from "./icon merge pdf.svg"
+import splitPDF from "./icon split pdf.svg"
+import compressPDF from "./icon compress pdf.svg"
+import jpgToPDF from "./icon jpg to pdf.svg"
+import excelToPDF from "./icon excel to pdf.svg"
+import pdfToWord from "./icon  pdf to word.svg"
+import pdfToJPG from "./icon pdf to jpg.svg"
+import pdfToExcel from "./icon pdf to excel.svg"
+import removeBackground from "./icon remove background.svg"
+
+export const ICON_MAP = {
+    wordToPDF           : wordToPDF,
+    mergePDF            : mergePDF,
+    splitPDF            : splitPDF,
+    compressPDF         : compressPDF,
+    jpgToPDF            : jpgToPDF,
+    excelToPDF          : excelToPDF,
+    pdfToWord           : pdfToWord,
+    pdfToJPG            : pdfToJPG,
+    pdfToExcel          : pdfToExcel,
+    removeBackground    : removeBackground
+}
