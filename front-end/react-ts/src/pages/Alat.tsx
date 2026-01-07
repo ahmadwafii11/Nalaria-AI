5367559977874107befd800c3beb80d8d9d470ae
ahmadwafii11 <wafiahmad950@gmail.com>
2026-01-07 15:09:04 +0700

    add active filteredType

--- front-end/react-ts/src/pages/Alat.tsx
+++ front-end/react-ts/src/pages/Alat.tsx
@@ -2,8 +2,16 @@ import "../index.css"
 import { NavLink } from "react-router-dom";
 import { alatList } from "../data/alatList";
 import { ICON_MAP } from "../assets/icons";
+import { useState } from "react";
 
 export default function Alat() {
+    type FilterType = "ALL" | "PDF" | "Word" | "Excel" | "JPG"
+    const [activeFilter, setActiveFilter] = useState<FilterType>("ALL");
+
+    const filteredTools = 
+        activeFilter === "ALL"
+            ? alatList
+            : alatList.filter((alat) => alat.category === activeFilter);
     return(
         <>
             <div className="content">
@@ -12,16 +20,22 @@ export default function Alat() {
 
                 {/*Filter Menu Alat*/}
                 <div className="tool-filter">
-                    <button className="active">ALL</button>
-                    <button>PDF</button>
-                    <button>WORD</button>
-                    <button>EXCEL</button>
-                    <button>JPG</button>
+                    {(["ALL", "PDF", "Word", "Excel", "JPG"] as FilterType[]).map(
+                        (filter) => (
+                            <button 
+                            key={filter}
+                            className={activeFilter === filter? "active" : ""}
+                            onClick={() => setActiveFilter(filter)}
+                            >
+                                {filter}
+                            </button>
+                        )
+                    )}
                 </div>
 
                 {/*Card Grid*/}
                 <div className="card-container">
-                    {alatList.map((alat) => (
+                    {filteredTools.map((alat) => (
                         <NavLink
                             key={alat.id}
                             to={`/alat/${alat.name}`}
